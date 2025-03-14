fb2888a221a8b6a7955de326d6f1aeed281717c2
Pabitra Gupta <pabitragupta947@gmail.com>
2025-03-14 20:09:07 +0530

    Thymleaf added

--- src/main/java/com/example/ToDo_App/controller/TaskController.java
+++ src/main/java/com/example/ToDo_App/controller/TaskController.java
@@ -1,15 +1,27 @@
 package com.example.ToDo_App.controller;
 
 
+import com.example.ToDo_App.model.Task;
 import com.example.ToDo_App.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
+//@RequestMapping("/tasks")
 public class TaskController {
 
     @Autowired
     private TaskService taskService;
 
-
+    @GetMapping("/tasks")
+    public String getTasks(Model model) {
+        List<Task> tasks = taskService.getAllTask();
+        model.addAttribute("tasks", tasks);
+        return "tasks";
+    }
 }
--- src/main/java/com/example/ToDo_App/service/TaskService.java
+++ src/main/java/com/example/ToDo_App/service/TaskService.java
@@ -1,8 +1,20 @@
 package com.example.ToDo_App.service;
 
 
+import com.example.ToDo_App.model.Task;
+import com.example.ToDo_App.respository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TaskService {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public List<Task> getAllTask(){
+        return taskRepository.findAll();
+    }
 }
